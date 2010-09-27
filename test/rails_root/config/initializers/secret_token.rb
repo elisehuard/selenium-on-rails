8c1af3d682031c3c24d48c2840ec41c55b311db8
Elise Huard <ehu@vasco.com>
2010-09-27 12:11:53 +0200

    minimal rails 3 application in tests instead of rails 2 app

--- test/rails_root/config/initializers/secret_token.rb
+++ test/rails_root/config/initializers/secret_token.rb
@@ -2,6 +2,6 @@
 
 # Your secret key for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
+# Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.cookie_verifier_secret = 'b1773cd36d07e852fc0d680ffaa09558bca23da6506a4ba58b3f34b36815f6ae71b7be9bb0fa4952e0865c958828d92df529edcea7e46b5b5dac3f45faacec53';
+RailsRoot::Application.config.secret_token = '99e6e6f9bd0b7de7c9f971ffd581a712bcddafea3886f82b7351704b61c560f982f2475e64e6f7996ab9b9f737d481b66c3b56e1d02dcd3e83d8ec7688e14f1a'
--- test/rails_root/config/initializers/session_store.rb
+++ test/rails_root/config/initializers/session_store.rb
@@ -1,15 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying cookie session data integrity.
-# If you change this key, all old sessions will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
-# no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.session = {
-  :key         => '_rails_root_session',
-  :secret      => 'f929ada6b9f725ae64ec8d1a676c386b82c1b9c9f9f5f200a5f4a69b02c3e3916a1e28afa6d3b1ca4af27aeb7e9bc36788bf35b4a59c2d5e33b754ecd8fdbceb'
-}
+RailsRoot::Application.config.session_store :cookie_store, :key => '_rails_root_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rake db:sessions:create")
-# ActionController::Base.session_store = :active_record_store
+# RailsRoot::Application.config.session_store :active_record_store
